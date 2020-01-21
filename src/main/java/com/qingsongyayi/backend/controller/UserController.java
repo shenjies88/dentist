14e284217db082067baec895d9ef7b73bc520926
shenjies88 <1961486254@qq.com>
2020-01-21 20:58:15 +0800

    feat:新增登陆接口

---
+++ src/main/java/com/qingsongyayi/backend/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.qingsongyayi.backend.controller;
+
+import com.qingsongyayi.backend.common.HttpResult;
+import com.qingsongyayi.backend.vo.LoginVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @author shenjies88
+ * @since 2020/1/21-8:48 PM
+ */
+@Api(tags = "用户信息接口")
+@AllArgsConstructor
+@RequestMapping("/api/user")
+@RestController
+public class UserController {
+
+    @ApiOperation("登陆")
+    @PostMapping("/login")
+    public HttpResult login(@RequestBody LoginVo body) {
+
+        if (body.getUserName().equals("admin") && body.getPassword().equals("password")) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("userName", body.getUserName());
+            map.put("password", body.getPassword());
+            map.put("token", (System.currentTimeMillis() + new Random().nextInt(999999999)) + "");
+            return HttpResult.success(map);
+        } else {
+            throw new RuntimeException("账号或者密码错误");
+        }
+    }
+}
---
+++ src/main/java/com/qingsongyayi/backend/vo/LoginVo.java
@@ -0,0 +1,15 @@
+package com.qingsongyayi.backend.vo;
+
+import lombok.Data;
+
+/**
+ * @author shenjies88
+ * @since 2020/1/21-8:49 PM
+ */
+@Data
+public class LoginVo {
+
+    private String userName;
+
+    private String password;
+}
