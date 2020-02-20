2166217d56426616baf294953f9a61e65ddf02f3
shenjies88 <1961486254@qq.com>
2020-02-20 14:24:31 +0800

    feat:构建推送镜像

--- docker_build.sh
+++ docker_build.sh
@@ -7,17 +7,17 @@ REPO=$5
 NAMESPACE=$6
 docker login -u ${PLUGIN_DOCKER_USERNAME} -p ${PLUGIN_DOCKER_PASSWORD} ${REPO};
 # mysql
-docker build --rm=true -f dockerfile-mysql -t ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
+docker build --rm=true -f dockerfile-mysql -t ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG} .;
 docker tag ${MYSQL_NAME} ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
 docker push ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
 docker rmi ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
 # 后端
-docker build --rm=true -f dockerfile-backend -t ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
+docker build --rm=true -f dockerfile-backend -t ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG} .;
 docker tag ${BACKEND_NAME} ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
 docker push ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
 docker rmi ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
 # 前端
-#docker build --rm=true -f dockerfile-backend -t ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
+#docker build --rm=true -f dockerfile-backend -t ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG} .;
 #docker tag ${FRONT_NAME} ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
 #docker push ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
 #docker rmi ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
