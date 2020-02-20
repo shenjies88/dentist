0dbb9ad739be710c9558148fe6e662446998920a
shenjies88 <1961486254@qq.com>
2020-02-20 15:20:47 +0800

    feat:构建推送镜像

--- docker_build.sh
+++ docker_build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-DOCKER_TAG=$1
-BACKEND_NAME=$2
-FRONT_NAME=$3
-MYSQL_NAME=$4
-REPO=$5
-NAMESPACE=$6
-docker login -u ${PLUGIN_DOCKER_USERNAME} -p ${PLUGIN_DOCKER_PASSWORD} ${REPO};
+version=$1
+backend_name=$2
+front_name=$3
+mysql_name=$4
+repo=$5
+namespace=$6
+docker login -u ${PLUGIN_DOCKER_USERNAME} -p ${PLUGIN_DOCKER_PASSWORD} ${repo};
 # mysql
-docker build --rm=true -f dockerfile-mysql -t ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG} .;
-docker tag ${MYSQL_NAME} ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
-docker push ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
-docker rmi ${REPO}/${NAMESPACE}/${MYSQL_NAME}:${DOCKER_TAG};
+docker build --rm=true -f dockerfile-mysql -t ${repo}/${namespace}/${mysql_name}:${version} .;
+docker tag ${mysql_name} ${repo}/${namespace}/${mysql_name}:${version};
+docker push ${repo}/${namespace}/${mysql_name}:${version};
+docker rmi ${repo}/${namespace}/${mysql_name}:${version};
 # 后端
-docker build --rm=true -f dockerfile-backend -t ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG} .;
-docker tag ${BACKEND_NAME} ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
-docker push ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
-docker rmi ${REPO}/${NAMESPACE}/${BACKEND_NAME}:${DOCKER_TAG};
+docker build --rm=true -f dockerfile-backend -t ${repo}/${namespace}/${backend_name}:${version} .;
+docker tag ${backend_name} ${repo}/${namespace}/${backend_name}:${version};
+docker push ${repo}/${namespace}/${backend_name}:${version};
+docker rmi ${repo}/${namespace}/${backend_name}:${version};
 # 前端
-#docker build --rm=true -f dockerfile-backend -t ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG} .;
-#docker tag ${FRONT_NAME} ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
-#docker push ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
-#docker rmi ${REPO}/${NAMESPACE}/${FRONT_NAME}:${DOCKER_TAG};
+#docker build --rm=true -f dockerfile-backend -t ${repo}/${namespace}/${front_name}:${version} .;
+#docker tag ${front_name} ${repo}/${namespace}/${front_name}:${version};
+#docker push ${repo}/${namespace}/${front_name}:${version};
+#docker rmi ${repo}/${namespace}/${front_name}:${version};
 
 docker system prune -f;
