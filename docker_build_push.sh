14c9ec5a9d8037d497871876d9d3271221c3b5cd
shenjies88 <1961486254@qq.com>
2020-02-23 20:48:11 +0800

    fix:部署服务

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -2,14 +2,14 @@
 version=$1
 backend_name=$2
 front_name=$3
-mysql_name=$4
-repo=$5
-namespace=$6
+#mysql_name=$4
+repo=$4
+namespace=$5
 docker login -u ${PLUGIN_DOCKER_USERNAME} -p ${PLUGIN_DOCKER_PASSWORD} ${repo};
 # mysql
-docker build --rm=true -f dockerfile-mysql -t ${repo}/${namespace}/${mysql_name}:${version} .;
-docker push ${repo}/${namespace}/${mysql_name}:${version};
-docker rmi ${repo}/${namespace}/${mysql_name}:${version};
+#docker build --rm=true -f dockerfile-mysql -t ${repo}/${namespace}/${mysql_name}:${version} .;
+#docker push ${repo}/${namespace}/${mysql_name}:${version};
+#docker rmi ${repo}/${namespace}/${mysql_name}:${version};
 # 后端
 docker build --rm=true -f dockerfile-backend -t ${repo}/${namespace}/${backend_name}:${version} .;
 docker push ${repo}/${namespace}/${backend_name}:${version};
