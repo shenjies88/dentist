7bcfdf77a8a20f298dcea2b34a70f95debb8a698
shenjies88 <1961486254@qq.com>
2020-03-07 20:45:38 +0800

    push

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -4,14 +4,16 @@ backend_name=$2
 front_name=$3
 repo=$4
 namespace=$5
-docker login -u ${PLUGIN_DOCKER_USERNAME} -p ${PLUGIN_DOCKER_PASSWORD} ${repo};
+account=$6
+pwd=$6
+docker login -u ${account} -p ${pwd} ${repo}
 # 后端
-docker build --rm=true -f dockerfile-backend -t ${repo}/${namespace}/${backend_name}:${version} .;
-docker push ${repo}/${namespace}/${backend_name}:${version};
-docker rmi ${repo}/${namespace}/${backend_name}:${version};
+docker build --rm=true -f dockerfile-backend -t ${repo}/${namespace}/${backend_name}:${version} .
+docker push ${repo}/${namespace}/${backend_name}:${version}
+docker rmi ${repo}/${namespace}/${backend_name}:${version}
 # 前端
-docker build --rm=true -f dockerfile-front -t ${repo}/${namespace}/${front_name}:${version} .;
-docker push ${repo}/${namespace}/${front_name}:${version};
-docker rmi ${repo}/${namespace}/${front_name}:${version};
+docker build --rm=true -f dockerfile-front -t ${repo}/${namespace}/${front_name}:${version} .
+docker push ${repo}/${namespace}/${front_name}:${version}
+docker rmi ${repo}/${namespace}/${front_name}:${version}
 docker volume prune -f
 docker system prune -f
