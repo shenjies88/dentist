2d036a64753a7ef40da3de5dcf2ca03cea4dd432
shenjies88 <1961486254@qq.com>
2020-03-08 11:45:57 +0800

    fix

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 version=$1
 backend_name=$2
-front_name=$3
+dockerfile=$3
 repo=$4
 namespace=$5
 account=$6
 pwd=$7
 docker login -u ${account} -p ${pwd} ${repo}
-# 后端
-docker build --rm=true -f dockerfile-backend -t ${repo}/${namespace}/${backend_name}:${version} .
+docker build --rm=true -f ${dockerfile} -t ${repo}/${namespace}/${backend_name}:${version} .
 docker push ${repo}/${namespace}/${backend_name}:${version}
 docker rmi ${repo}/${namespace}/${backend_name}:${version}
-# 前端
-docker build --rm=true -f dockerfile-front -t ${repo}/${namespace}/${front_name}:${version} .
-docker push ${repo}/${namespace}/${front_name}:${version}
-docker rmi ${repo}/${namespace}/${front_name}:${version}
 docker volume prune -f
 docker system prune -f
